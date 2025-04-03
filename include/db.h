f4387a283546e06ac69fd2d7407e4d8b3e031170
Nick Tan <tnick21@vt.edu>
2025-04-03 14:26:32 -0400

    added singlton repo to new repo

---
+++ include/db.h
@@ -0,0 +1,82 @@
+//db.h
+#pragma once
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <new>
+
+using namespace std;
+
+class Database{
+private:
+	string db;
+	string username;
+	string password;
+	string connected = "false";
+	static Database* instance;
+	
+	Database(const string DB, const string UN, const string PW) :
+		db{ DB }, username{ UN }, password{ PW }
+	{}
+
+	~Database()
+	{
+		if (isConnected())
+		{
+			cout << "Disconnect connection" << endl;
+			connected = "false";
+		}
+	}
+
+public:
+
+	//"getInstance" that creates and returns the instance of the database. 
+	// If called first time it sets the username and password. 
+	// However, subsequent time, it matches the database name, 
+	// username and password and returns the previous instance 
+	// if matched else it throws std::runtime_error("invalid database name, username or password"). 
+	// We are using Singleton Design Pattern that creates only one instance of the databse. The instance is still created by the constructor.
+	//ToDo
+	static Database* getInstance(string DB, string UN, string PW)
+	{
+		if (!instance)
+		{
+			instance = new Database(DB, UN, PW);
+		}
+		else if (instance->db != DB || instance->username != UN || instance->password != PW) {
+			throw std::runtime_error("invalid database name, username or password");
+		}
+
+		return instance;
+	}
+
+	//"connect" that sets "connected" to true (return void)
+	void connect();
+
+	//"connect" that sets "connected" to true (return void)
+	void disconnect();
+
+	// retrun status of connected true/false (return bool)
+	bool isConnected();
+
+	//overload the new operator that allocates memory using malloc 
+	// of given size and returns pointer of type void and prints " overloaded new " 
+	// (cout is okay in this case). std::cout << "overloaded new ";
+	//If the memory allocation fails it should throw std::bad_alloc()
+	void* operator new(size_t size);
+
+	//overload the delete operator that deallocates memory and prints 
+	// "overloaded delete " (cout is okay in this). std::cout << "overloaded delete ";
+	void operator delete(void* ptr);
+
+	//set_username and get_username for username
+	string get_username() const;
+	void set_username(const string newUN);
+
+	//set_password and get_password for password.
+	string get_password() const;
+	void set_password(const string newPW);
+
+	//The static "resetInstance" as defined below.
+	static void resetInstance();
+};
---
+++ src/db.cpp
@@ -0,0 +1,63 @@
+//db.cpp
+
+#include "db.h"
+
+Database* Database::instance = nullptr;
+
+
+
+void Database::connect()
+{
+    connected = "true";
+}
+
+void Database::disconnect()
+{
+    connected = "false";
+}
+
+bool Database::isConnected()
+{
+    return connected == "true";
+}   
+
+void* Database::operator new(size_t size)
+{
+    std::cout << "overloaded new ";
+    void* ptr = malloc(size);
+    if (!ptr) { throw std::bad_alloc(); }
+    return ptr;
+}
+
+void Database::operator delete(void* ptr)
+{
+    std::cout << "overloaded delete ";
+    free(ptr);
+}
+
+string Database::get_username() const
+{
+    return username;
+}
+
+void Database::set_username(const string newUN)
+{
+    this->username = newUN;
+}
+
+string Database::get_password() const
+{
+    return password;
+}
+
+void Database::set_password(const string newUN)
+{
+    this->password = newUN;
+}
+
+void Database::resetInstance() {
+    if (instance != nullptr) {
+        delete instance;
+        instance = nullptr;
+    }
+}
